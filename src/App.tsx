04a2f6bf1d398ef6f0b26a4a3836a46af904b4d8
Tima <dadeev.tim@gmail.com>
2022-06-20 21:15:15 +0700

    finish almost final tasck(lesson 04)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    console.log('App rendering')
+    return (
+        <div className="App">
+            {/*<PageTitle title={'This is APP component'}/>*/}
+            {/*<PageTitle title={'My friends'}/>*/}
+            {/*Article1*/}
+            {/*<Rating value={3}/>*/}
+            <Accordion titleValue={'Menu'} collapsed={false}/>
+            <Accordion titleValue={'Users'} collapsed={true}/>
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+        </div>
+    );
 }
+
+type PageTitlePropsType = {
+    title: string;
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    console.log('AppTitle rendering')
+    return <h1>{props.title}</h1>
+}
+
 
 export default App;
