a508463f2cae9d441947c71f20fba57c16ebc70b
Tima <dadeev.tim@gmail.com>
2022-06-23 19:32:57 +0700

    refactor: correct some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Button} from "./components/Button";
+
+
+type FilterType = 'all' | 'RUBLS' | 'Dollars';
 
 function App() {
-    const Button1Foo = (sub: string, age: number, address: string) => {
-        console.log(sub, age, address)
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+
+const [filter, setFilter] = useState<FilterType>('all')
+
+    let currentMoney = money;
+    if (filter === 'RUBLS') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS')
     }
-    const Button2Foo = (sub: string) => {
-        console.log(sub)
+    if (filter === 'Dollars') {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'Dollars')
     }
-    const onClickHandler = () => {
-        console.log('I am stupid button')
+
+    const onClickFilterHandler = (nameButton: FilterType) => {
+        setFilter(nameButton)
     }
+
     return (
         <>
-            {/*<button>MyYoutubeChannel-1</button>*/}
-            {/*<button>MyYoutubeChannel-2</button>*/}
-            <Button name={'MyYoutubeChannel-1'} callBack={() => Button1Foo('I am Vasya', 16, 'live in Russia')}/>
-            <Button name={'MyYoutubeChannel-2'} callBack={() => Button2Foo('I am Ivan')}/>
-            <Button name={'stupid'} callBack={onClickHandler}/>
+            <ul>
+                {currentMoney.map((el, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{el.banknots}</span>
+                            <span>{el.value}</span>
+                            <span>{el.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div style={{marginLeft: '35px'}}>
+                <button onClick={() => onClickFilterHandler ('all')}>all</button>
+                <button onClick={() => onClickFilterHandler ('RUBLS')}>ruble</button>
+                <button onClick={() => onClickFilterHandler ('Dollars')}>dollar</button>
+            </div>
         </>
     )
 }
