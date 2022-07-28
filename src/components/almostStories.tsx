083d3ff1f4a0950403767fa514258c373581a493
Tima <dadeev.tim@gmail.com>
2022-07-28 19:14:26 +0700

    feat: imolement controlled input, checkbox and select

--- src/components/almostStories.tsx
+++ src/components/almostStories.tsx
@@ -1,4 +1,5 @@
 import {ChangeEvent, useRef, useState} from "react";
+import {action} from "@storybook/addon-actions";
 
 export const UncontrolledInput = () => <input/>
 export const TrackValueOfUncontrolledInput = () => {
@@ -26,5 +27,33 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
     </>
 }
 
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChangeHandler}/>
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
+    return <input type={"checkbox"} checked={parentValue} onChange={onChangeHandler}/>
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+    const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <select value={parentValue} onChange={onChangeHandler}>
+        <option>none</option>
+        <option value="1">Minsk</option>
+        <option value="2">Moscow</option>
+        <option value="3">Kiev</option>
+    </select>
+}
 export const ControlledInputWithSomeValue = () => <input value={'it-incubator'}/>
 
